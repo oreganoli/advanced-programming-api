ba1ad1dcfb76572ce46c6b98b7ded5e6ef195ebe
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-11-19 16:54:58 +0100

    remove example endpoints

--- main.py
+++ main.py
@@ -7,12 +7,3 @@ app = FastAPI()
 @app.get("/prime/{num}")
 async def is_prime(num: int) -> bool:
     return sympy.isprime(num)
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
