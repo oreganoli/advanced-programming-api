cb57cfda39bc4e5b362adfbd9d4f7111ffac4168
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-11-19 20:12:57 +0100

    add task 3

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import cv2
-from http import HTTPStatus
+import jwt
 import sympy
 import numpy as np
-from fastapi import FastAPI, UploadFile, HTTPException
+import time
+from http import HTTPStatus
+from fastapi import FastAPI, Header, UploadFile, HTTPException
 from fastapi.responses import Response
+from pydantic import BaseModel
+
+
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
 
 app = FastAPI()
 
@@ -39,3 +48,27 @@ async def invert(file: UploadFile) -> Response:
         raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
                             detail="The output image could not be successfully encoded.")
     return Response(content=output.tobytes(), media_type="image/jpeg")
+
+
+@app.get("/time")
+async def get_time(authorization: str | None = Header(default=None)):
+    if authorization is None:
+        raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED,
+                            detail="No authorization was provided.")
+    try:
+        claims = jwt.decode(
+            authorization, "TOP SEEKRIT DONUT STEEL", algorithms=["HS256"])
+    except:
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST,
+                            detail="The authorization token provided was malformed or forged and could not be decoded.")
+    if claims["privileges"] is None or claims["privileges"].find("get_time") == -1:
+        raise HTTPException(status_code=HTTPStatus.FORBIDDEN,
+                            detail="You are not allowed to perform this operation.")
+    return time.time()
+
+
+@app.post("/login")
+async def login(req: LoginRequest) -> str | None:
+    if req.username == "EXAMPLE_USER" and req.password == "EXAMPLE_PASSWORD":
+        return jwt.encode({"user": "EXAMPLE_USER", "privileges": "get_time"}, "TOP SEEKRIT DONUT STEEL")
+    return None
